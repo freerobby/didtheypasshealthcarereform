cfbb0cc8eb1914261077ce14024a8b71988c0c10
Robby Grossman <robby@freerobby.com>
2010-03-21 19:22:55 -0400

    Move contingencies to constants and create html header/footer.

--- app.rb
+++ app.rb
@@ -1,22 +1,31 @@
 require 'rubygems'
 require 'sinatra'
 
+HEADER = '
+<html>
+  <head>
+    <title>Did they pass health care reform?</title>
+  </head>
+  <body>
+'
+
+FOOTER = '
+  </body>
+</html>
+'
+
+NO = '<div style="text-align: center; font-size: 6em;">no</div>'
+
+YES = '
+<div style="text-align: center; font-size: 6em;">yes!</div>
+<script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>
+<script language="javascript">
+  for (var i = 0; i < 20; i++) {
+    cornify_add();
+  }
+</script>
+'
+
 get '/' do
-  no
-end
-
-def no
-  '<div style="text-align: center; font-size: 6em;">no</div>'
-end
-
-def yes
-  '
-  <div style="text-align: center; font-size: 6em;">yes!</div>
-  <script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>
-  <script language="javascript">
-    for (var i = 0; i < 20; i++) {
-      cornify_add();
-    }
-  </script>
-  '
+  HEADER + NO + FOOTER
 end
