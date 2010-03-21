4b3d1d7fd6835721cdff7c16793fbc61c1d8452b
Robby Grossman <robby@freerobby.com>
2010-03-21 19:53:49 -0400

    Add Google Analytics.

--- app.rb
+++ app.rb
@@ -9,14 +9,26 @@ HEADER = '
   <body>
 '
 
+GA = '
+  <script type="text/javascript">
+  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+  document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+  </script>
+  <script type="text/javascript">
+  try {
+  var pageTracker = _gat._getTracker("UA-15385027-1");
+  pageTracker._trackPageview();
+  } catch(err) {}</script>
+'
+
 FOOTER = '
   </body>
 </html>
 '
 
-NO = '<div style="text-align: center; font-size: 6em;">no</div>'
+NO = HEADER + '<div style="text-align: center; font-size: 6em;">no</div>' + GA + FOOTER
 
-YES = '
+YES = HEADER + '
 <div style="text-align: center; font-size: 6em;">yes!</div>
 <script type="text/javascript" src="/cornify.js"></script>
 <script language="javascript">
@@ -24,8 +36,8 @@ YES = '
     cornify_add();
   }
 </script>
-'
+' + GA + FOOTER
 
 get '/' do
-  HEADER + NO + FOOTER
+  NO
 end
