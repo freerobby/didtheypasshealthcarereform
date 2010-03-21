e2c67f9753272e5cd7d562fe35d5b1fe459a5902
Robby Grossman <robby@freerobby.com>
2010-03-21 19:11:49 -0400

    Complete no and yes contingencies.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "<div style='text-align: center; font-size: 6em;'>no</div>"
+  no
+end
+
+def no
+  '<div style="text-align: center; font-size: 6em;">no</div>'
+end
+
+def yes
+  '
+  <div style="text-align: center; font-size: 6em;">yes!</div>
+  <script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>
+  <script language="javascript">
+    for (var i = 0; i < 20; i++) {
+      cornify_add();
+    }
+  </script>
+  '
 end
